a5dda1c8b5ca930cc5205fc3a70b6433009ef89e
Gregory Schier <gschier1990@gmail.com>
2016-11-08 19:17:52 -0800

    Finished port and tests

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # go-srp
-Port of Mozilla node-srp to Go
+
+This is a port of [node-srp](https://github.com/mozilla/node-srp) to Go.
+
+## Installation
+
+```
+go get github.com/getinsomnia/go-srp
+```
+
+## Running Tests
+
+```
+go test
+```
+
+_Tests include vectors from 
+[RFC 5054, Appendix B.](https://tools.ietf.org/html/rfc5054#appendix-B)_
+
+## How to Use It
+
+TODO... 
+
+For now, See `srp_tests.go` for usage.
+
+## Licence
+
+MIT
---
+++ srp.go
@@ -0,0 +1,58 @@
+package srp
+
+import (
+	"crypto/rand"
+	"io"
+	"math/big"
+)
+
+
+func GenKey(numBytes int) []byte {
+	bytes := make([]byte, numBytes)
+	_, err := io.ReadFull(rand.Reader, bytes)
+	if err != nil {
+		panic("Random source is broken!")
+	}
+
+	return bytes
+}
+
+func getK(params *SRPParams, S []byte) []byte {
+	hashK := params.Hash.New()
+	hashK.Write(S)
+	return hashToBytes(hashK)
+}
+
+
+func getu(params *SRPParams, A, B *big.Int) *big.Int {
+	hashU := params.Hash.New()
+	hashU.Write(A.Bytes())
+	hashU.Write(B.Bytes())
+
+	return hashToInt(hashU)
+}
+
+func getM1(params *SRPParams, A, B, S []byte) []byte {
+	hashM1 := params.Hash.New()
+	hashM1.Write(A)
+	hashM1.Write(B)
+	hashM1.Write(S)
+	return hashToBytes(hashM1)
+}
+
+func getM2(params *SRPParams, A, M, K []byte) []byte {
+	hashM1 := params.Hash.New()
+	hashM1.Write(A)
+	hashM1.Write(M)
+	hashM1.Write(K)
+	return hashToBytes(hashM1)
+}
+
+
+func getMultiplier(params *SRPParams) *big.Int {
+	hashK := params.Hash.New()
+	hashK.Write(padToN(params.N, params))
+	hashK.Write(padToN(params.G, params))
+
+	return hashToInt(hashK)
+}
